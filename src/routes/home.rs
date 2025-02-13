4e3c9b10f62db48b456a1b900860d010a137e9b3
Starman <kubanychbekukairat@gmail.com>
2025-02-13 17:20:54 +0600

    [REMOVE] delete unused routes, configuration files, and Dockerfiles; add new service structure

--- src/routes/home.rs
+++
@@ -1,8 +0,0 @@
-#![allow(dead_code, unused_imports)]
-use actix_web::Responder;
-use actix_files::NamedFile;
-
-
-pub async fn home_page() -> impl Responder {
-    NamedFile::open("front/home/index.html").unwrap()
-}
